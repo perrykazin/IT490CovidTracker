d89a0c69ac656eab44b06cf8b9c9034f59b15654
Puhalka <64510849+Puhalka@users.noreply.github.com>
2020-10-19 23:37:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -27,27 +27,27 @@
 		
 		<table>
       
-      <tr>
+			<tr>
 				<td align = "right">User Name</td>
 				<td><input type = "text" id="email" name = "email" size="50" /></td>
-				<span id=”email”></span>
-        <td style="color:red">REQUIRED</td>
+				<span id=â€emailâ€></span>
+				<td style="color:red">REQUIRED</td>
 			</tr>
       
-      <tr>
-        <td align = "right">Password</td>
-        <td><input type = "password" id="password" name ="password" size="50"/></td>
-        <span id=”password”></span>
-        <td style="color:red">REQUIRED</td>
-      </tr>
-      
-      <tr>
-       <td align = "right"><input type = "reset" value = "Reset" /></td>
-       <td align = "center"><input type = "submit" value = "Submit" /></td>	
-     </tr>
-     </table>
+			<tr>
+				<td align = "right">Password</td>
+				<td><input type = "password" id="password" name ="password" size="50"/></td>
+				<span id=â€passwordâ€></span>
+				<td style="color:red">REQUIRED</td>
+			</tr>
+		  
+			<tr>
+				<td align = "right"><input type = "reset" value = "Reset" /></td>
+				<td align = "center"><input type = "submit" value = "Submit" /></td>	
+			</tr>
+		</table>
 
-  </form>
+	</form>
   
   </div>
   
@@ -59,8 +59,7 @@
     </form>
   
   </div>
-
-  <?php
+/* <?php
  
    require_once("connection.php");
    
@@ -94,7 +93,38 @@
    }
  
    $conn->close();
- ?>
+ ?> */
+ 
+  <?php
+    require_once('path.inc');
+    require_once('get_host_info.inc');
+    require_once('rabbitMQLib.inc');
+
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    if (isset($argv[1]))
+    {
+      $msg = $argv[1];
+    }
+    else
+    {
+      $msg = "test message";
+    }
+
+    $request = array();
+    $request['type'] = "Login";
+    $request['username'] = "steve";
+    $request['password'] = "password";
+    $request['message'] = $msg;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+
+    echo "client received response: ".PHP_EOL;
+    print_r($response);
+    echo "\n\n";
+
+    echo $argv[0]." END".PHP_EOL;
+  ?>
+
 
 </body>
 
