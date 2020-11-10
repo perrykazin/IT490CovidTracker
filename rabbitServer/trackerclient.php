41cfcd7a8a2c6e4dbc6f7b8d8db43418759060ec
perrykazin <54904249+perrykazin@users.noreply.github.com>
2020-11-10 02:38:52 +0000

    Add files via upload

--- rabbitServer/trackerclient.php
+++ rabbitServer/trackerclient.php
@@ -104,4 +104,22 @@ function rabbitAPIpush($cases, $deaths){
     echo $argv[0]." END".PHP_EOL;
 }
 
+//test functionality
+$client = new RabbitMQClient('messageServer.ini', 'messageServer');
+if(isset($argv[1])){
+	$msg = $argv[1];
+}
+else{
+	$msg = array("message"=>"test message", "type"=>"echo");
+}
+
+$response = $client->send_request($msg);
+
+echo "client received response: " . PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+if(isset($argv[0]))
+echo $argv[0] . " END".PHP_EOL;
+
 ?>
