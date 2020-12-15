6354ba33b346c72d339c00025a2135bd47123168
perrykazin <54904249+perrykazin@users.noreply.github.com>
2020-12-15 15:23:02 -0500

    Add files via upload

--- msgserver.php
+++ msgserver.php
@@ -5,7 +5,7 @@ require_once('rabbitMQLib.inc');
 
 function loginpass($username,$password)
 {
-    $dbconnect = new mysqli('10.1.0.5','root','it490Group2!!!','user_credentials', '');
+    $dbconnect = new mysqli('10.1.0.5','root','it490Group2!!!','user_credentials');
 	
 	if ($dbconnect->errno != 0){
 
@@ -47,13 +47,13 @@ function registerpass($username,$password)
         exit(0);
     }
     else {
-        echo "Successfully connected to database".PHP_EOL;
+        echo "Successfully connected to dat=base".PHP_EOL;
     }
 
     $checkquery = mysqli_query($dbconnect,"SELECT * FROM users WHERE username = '$username' ");
 	$count = mysqli_num_rows($checkquery);
 
-    if ($count != 0){
+    if ($count == 0){
         $query = mysqli_query($dbconnect,"INSERT INTO users (username, password) VALUES ('$username', '$password')");
         echo "Account created".PHP_EOL;
         return true;
@@ -100,6 +100,13 @@ function apipass ($uscases,$usdeaths,$njcases,$njdeaths,$nycases,$nydeaths)
     }
 
     $query = mysqli_query($dbconnect,"INSERT INTO corona_stats (total_cases, total_deaths, nj_cases, nj_deaths, ny_cases, ny_deaths) VALUES ('$uscases', '$usdeaths', '$njcases', '$njdeaths', '$nycases', '$nydeaths')");
+    
+    if ($query->errno !=0){
+        echo "Failed to execute query: ".PHP_EOL;
+        echo __FILE__.':'.__LINE__.":error: ".$dbconnect->error.PHP_EOL;
+        exit(0);
+    }
+    
     echo "Database updated".PHP_EOL;
     return true;
 
@@ -128,7 +135,7 @@ function request_processor($request){
         case "populate":
             return populatepass();
         case "apipush":
-            return apipass($request['cases'], $request['deaths']);
+            return apipass($request['uscases'], $request['usdeaths'], $request['njcases'], $request['njdeaths'], $request['nycases'], $request['nydeaths']);
     }
     
 	return array("return_code" => '0',
